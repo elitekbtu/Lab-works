6b67fef1352a0892c35bc24b2b700f23066b69d1
elitekbtu <satbaldievst@mail.ru>
2024-01-22 00:06:12 +0600

    last version

---
+++ lab1/string.py
@@ -0,0 +1,25 @@
+# task 1
+x = "Hello World"
+print(len(x))
+# task 2
+txt="Hello world"
+print(txt[0])
+# task 3
+txt="Hello world"
+print(txt[2:5])
+# task 4
+txt = " Hello World "
+print(txt.strip())
+# task 5
+txt = "Hello World"
+txt = txt.upper()
+# task 6
+txt = "Hello World"
+txt = txt.lower()
+# task 7
+txt="Hello World"
+txt=txt.replace("H", "J")
+# task 8
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
