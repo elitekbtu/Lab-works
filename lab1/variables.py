916f2e2e5c2c3129838c2c77bc05ae741419042a
elitekbtu <satbaldievst@mail.ru>
2024-01-21 23:33:16 +0600

    last version

--- lab1/variables.py
+++ lab1/variables.py
@@ -11,3 +11,22 @@ x=5
 y=10
 print(x+y)
 
+#task 4
+
+x = 5
+y = 10
+z = x + y
+print(z)
+
+#task 5
+x, y, z = "Orange", "Banana", "Cherry"
+
+#task 6
+
+x=y=z="Orange"
+
+#task 7
+
+def myfunc():
+    global x
+    x = "fantastic"
