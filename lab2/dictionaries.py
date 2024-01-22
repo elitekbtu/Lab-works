50b302506280b7d6f2b3c35ba63e846219c30225
elitekbtu <satbaldievst@mail.ru>
2024-01-23 02:22:22 +0600

    last

---
+++ lab2/dictionaries.py
@@ -0,0 +1,14 @@
+#task  1
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(
+car.get("model"))
+
+#task 2
+
+#task 3
+#task 4
+#task 5
---
+++ lab2/sets.py
@@ -0,0 +1,21 @@
+# task 1 
+fruits = {"apple", "banana", "cherry"}
+if "apple" in  fruits:
+    print("Yes, apple is a fruit!")
+# task 2
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+
+# task 3
+fruits = {"apple", "banana", "cherry"}
+more_fruits = ["orange", "mango", "grapes"]
+fruits.update(more_fruits)
+
+# task 4
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+
+# task 5
+fruits = {"apple", "banana", "cherry"}
+fruits.discard("banana")
+
