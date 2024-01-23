77efd792eb42488d43e4ae2042e62fee5931205e
elitekbtu <satbaldievst@mail.ru>
2024-01-23 18:42:55 +0600

    last commit

---
+++ lab2/forloop.py
@@ -0,0 +1,19 @@
+# task 1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+# task 2
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+      continue
+    print(x)
+# task 3
+for x in range(6):
+  print(x)
+# task 4
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
---
+++ lab2/while.py
@@ -0,0 +1,26 @@
+# task 1
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+# task 
+i = 1
+while i < 6:
+  if i == 3:
+    break
+  i += 1
+# task 3
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+    print(i)
+# task 4
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
