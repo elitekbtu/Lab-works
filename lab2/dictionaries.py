5e48e5a009ee262cc822655b2b82581c84b93218
elitekbtu <satbaldievst@mail.ru>
2024-01-23 18:11:21 +0600

    last commit

--- lab2/dictionaries.py
+++ lab2/dictionaries.py
@@ -8,7 +8,34 @@ print(
 car.get("model"))
 
 #task 2
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
 
 #task 3
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"] = "red"
+
 #task 4
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+
 #task 5
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+
---
+++ lab2/ifelse.py
@@ -0,0 +1,49 @@
+# task 1
+a = 50
+b = 10
+if a > b:
+    print("Hello World")
+# task 2
+a = 50
+b = 10
+if a != b:
+    print("Hello World")
+# task 3
+a = 50
+b = 10
+if a==b:
+    print("Yes")
+else:
+    print("No")
+# task 4
+a = 50
+b = 10
+if a==b:
+   print("1")
+elif a>b:
+    print("2")
+else:
+    print("3")
+# task 5
+    
+
+if a == b and c == d:
+    print("Hello")
+# task 6
+if a == b or c == d:
+    print("Hello")
+# task 7
+if 5 > 2:
+    print("YES")
+
+# task 8
+a=2
+b=5
+
+print("YES") if a == b else  print("NO")
+# task 9
+a = 2
+b = 50
+c = 2
+if a == c or b == c:
+    print("YES")
