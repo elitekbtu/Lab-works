f7807f5557461b0f43d6688259eb553a624769b8
elitekbtu <satbaldievst@mail.ru>
2024-02-03 13:41:37 +0600

    last version generators

--- lab4/generators.py
+++ lab4/generators.py
@@ -22,6 +22,48 @@ def evens(n):
 print(evens(n))
 
 
+def div34(n):
+    result =[0]
+    for i in range(3, n+1):
+        c = i
+        while c>1:
+            if c%3==0:
+                c/=3 
+            elif c%4==0:
+                c/=4
+            else:
+                break
+
+        if c==1:
+            result.append(i)
+    return result
+
+n = int(input("please input n: "))
+print(div34(n))
+
+a, b = map(int, input("please input a and b: ").split())
+
+
+def squares(n, m):
+    for i in range(n, m+1):
+        yield i**2
+    
+generator = squares(a, b)
+for i in generator:
+    print(i)
+
+def all(n):
+    for i in range(n, -1, -1):
+        yield i
+
+n = int(input("please input n: "))    
+generator = all(n)
+
+for i in generator:
+    print(i)
+
+
+
 
 
 
