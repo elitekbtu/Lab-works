239eccf5b92c7a3a5216e19c0042390aec72e08f
elitekbtu <satbaldievst@mail.ru>
2024-02-15 02:33:18 +0600

    lab6

---
+++ lab6/builtinfunc.py
@@ -0,0 +1,34 @@
+def multi(array):
+    s = 1
+    for i in range(len(array)//2):
+        s*=array[i]
+        s*=array[len(array)-1-i]
+        if len(array)%2==1 and i==len(array)//2-1:
+            s*=array[i+1]
+    return s
+
+
+def sumofletters(some_s):
+    uppercase = 0
+    lowercase = 0
+    for i in some_s:
+        if i>='A' and i<='Z':
+            uppercase+=1
+        elif i>='a' and i<='z':
+            lowercase+=1
+    result = [lowercase, uppercase]
+    return result
+
+
+
+
+def palindrom(some_s):
+    if some_s==some_s[::-1]:
+        return f"the word {some_s} is palindrom"
+    else:
+        return f"the word {some_s} isn't palindrom"
+    
+
+
+
+        
---
+++ lab6/tester.py
@@ -0,0 +1,14 @@
+import builtinfunc as func
+#test func 1
+
+print(func.multi([1,2,3,4,5]))
+
+#test func 2
+
+print(func.sumofletters("abcdABCDabcd"))
+
+#test func 3
+
+print(func.palindrom("qazaq"))
+
+print(func.palindrom("racecar"))
