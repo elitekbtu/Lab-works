0ec64f4c528dd301862755c95c0ae1df09b09c77
elitekbtu <satbaldievst@mail.ru>
2024-02-16 03:35:26 +0600

    lab6 75%

--- lab6/dir-files.py
+++ lab6/dir-files.py
@@ -1,1 +1,38 @@
-print("Hello World")
+import os 
+
+#task 1
+
+path = r"C:\Users\Турарбек\Documents"
+all = list(os.listdir(path))
+print(all)
+
+#task 2
+
+def check_access(path):
+    if not os.path.exists(path):
+        print("path doesn't exist")
+        return 
+    else:
+        print('path does exist')
+        if os.access(path, os.R_OK):
+            print("readable")
+        else:
+            print("don't readable")
+        if os.access(path, os.W_OK):
+            print("writable")
+        else:
+            print("don't writable")
+        if os.access(path, os.X_OK):
+            print("executable")
+        else:
+            print("don't executable")
+
+if __name__ == "__main__":
+    path_to_check = r"C:\Users\Турарбек\Documents"
+    
+check_access(path_to_check)
+
+#task 3
+
+
+
