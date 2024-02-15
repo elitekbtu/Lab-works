70cee34104f33fa347b4aa642fdeff9ae46f86ed
elitekbtu <satbaldievst@mail.ru>
2024-02-16 04:48:11 +0600

    lab6

--- lab6/dir-files.py
+++ lab6/dir-files.py
@@ -1,4 +1,5 @@
 import os 
+import string
 
 #task 1
 
@@ -34,5 +35,65 @@ check_access(path_to_check)
 
 #task 3
 
+def checker(path):
+    if os.path.exists(path):
+        print("Name of file: ", os.path.basename(path))
+        print("name of directory: ", os.path.dirname(path))
+        return "success"
+    
+print(checker(path))
 
 
+# task 4
+
+with open("sometext.txt") as f:
+    data = f.read()  
+
+print(len(list(data.split("\n"))))
+f.close()
+
+#task 5
+
+def writesome(list_of_elements):
+    with open("sometext.txt", '+a') as f:
+        text = "\n"
+        for i in list_of_elements:
+            text+=str(i)+' '
+        f.write(text)
+        f.close()
+    return 0
+
+writesome([12345, 56789, 90987654, "dfghjkl"])
+
+# task 6
+
+import string
+
+def generate_files():
+    for letter in string.ascii_uppercase:
+        filename = letter + ".txt"
+        with open(filename, 'w') as file:
+            file.write("hello world")
+
+if __name__ == "__main__":
+    generate_files()
+
+#task 7
+
+def copier():
+    string = str(input("Enter the name of file: "))
+    with open(string) as file:
+        data = file.read()
+    file.close()
+    copy_path = ""
+    for i in range(len(string)):
+        if string[i]=='.':
+            copy_path+='_1'
+        copy_path+=string[i]
+    with open(copy_path, "+w") as file_copy:
+        file_copy.write(data)
+    file.close()
+    
+    return 0
+
+copier()
