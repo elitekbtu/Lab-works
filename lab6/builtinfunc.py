588256ddd473d79eb133b975ace7700ca2a32503
elitekbtu <satbaldievst@mail.ru>
2024-02-27 01:59:55 +0600

    last version

--- lab6/builtinfunc.py
+++ lab6/builtinfunc.py
@@ -30,10 +30,12 @@ def palindrom(some_s):
     
 
 def squareroot():
+    import time
     import math
     x = int(input("Input the value: "))
     y = int(input("Input the miliseconds: "))
-
+    y=y/1000
+    time.sleep(y)
     return f"Square root of {x} after {y} miliseconds is {math.sqrt(x)}"
     
 
